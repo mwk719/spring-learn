80671e19208e0d4803d2ca0e415b4f6445bc5095
minweikai <1294608448@qq.com>
2019-12-04 10:02:11 +0800

    add 集合求和

--- src/main/java/com/mwk/entity/Person.java
+++ src/main/java/com/mwk/entity/Person.java
@@ -1,6 +1,23 @@
 package com.mwk.entity;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class Person {
+
+    private String name;
+
+    private BigDecimal money;
+
+    public Person() {
+    }
+
+    public Person(String name, BigDecimal money) {
+        this.name = name;
+        this.money = money;
+    }
 
     public void say() {
         System.out.println("hello person");
---
+++ src/main/java/com/mwk/stream/sum.java
@@ -0,0 +1,44 @@
+package com.mwk.stream;
+
+import com.mwk.entity.Person;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class sum {
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1, 4, 6, 2, 8, 5);
+
+		//统计求和
+		IntSummaryStatistics collect = list.stream().collect(Collectors.summarizingInt(value -> value));
+		System.out.println(collect);
+		System.out.println("统计集合元素的个数：" + collect.getCount());
+		System.out.println("集合元素累加之和：" + collect.getSum());
+		System.out.println("集合中最小值：" + collect.getMax());
+		System.out.println("集合中最大值：" + collect.getMin());
+		System.out.println("集合中平均值：" + collect.getAverage());
+
+		Person p1 = new Person("张三", new BigDecimal("10.0"));
+		Person p2 = new Person("王五", new BigDecimal("10.0"));
+		Person p3 = new Person("李四", new BigDecimal("10.0"));
+		Person p4 = new Person("王五", new BigDecimal("10.0"));
+		Person p5 = new Person("赵六", new BigDecimal("10.0"));
+		List<Person> pList = new ArrayList<>();
+		pList.add(p1);
+		pList.add(p2);
+		pList.add(p3);
+		pList.add(p4);
+		pList.add(p5);
+
+		//对象属性求和
+		BigDecimal reduce = pList.stream().map(Person::getMoney).reduce(BigDecimal.ZERO, BigDecimal::add);
+		System.out.println("工资总计：" + reduce);
+
+	}
+
+}
