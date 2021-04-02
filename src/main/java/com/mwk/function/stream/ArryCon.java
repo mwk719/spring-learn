9148fa74089648a653dfbc04d275176a34984fd1
minwk <1294608448@qq.com>
2021-04-02 08:53:50 +0800

    add 交集、并集、茶几

---
+++ src/main/java/com/mwk/function/stream/ArryCon.java
@@ -0,0 +1,67 @@
+package com.mwk.function.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 交集、并集、茶几
+ * @author MinWeikai
+ * @date 2021/4/1 18:37
+ */
+public class ArryCon {
+
+    public static void main(String[] args) {
+        /*一般有filter 操作时，不用并行流parallelStream ,如果用的话可能会导致线程安全问题
+　　     判断对象要重写hash*/
+
+        List<String> list1 = new ArrayList();
+        list1.add("1111");
+        list1.add("2222");
+        list1.add("3333");
+
+        List<String> list2 = new ArrayList();
+        list2.add("3333");
+        list2.add("4444");
+        list2.add("5555");
+
+
+        // 交集
+        List<String> intersection = list1.stream().filter(item -> list2.contains(item)).collect(Collectors.toList());
+        System.out.println("---得到交集 intersection---");
+        intersection.parallelStream().forEach(System.out::println);
+
+        //同双冒号写法
+        intersection.parallelStream().forEach(x->{
+            System.out.println("x ="+x);
+        });
+
+        // 差集 (list1 - list2)
+        List<String> reduce1 = list1.stream().filter(item -> !list2.contains(item)).collect(Collectors.toList());
+        System.out.println("---得到差集 reduce1 (list1 - list2)---");
+        reduce1.parallelStream().forEach(System.out::println);
+
+        // 差集 (list2 - list1)
+        List<String> reduce2 = list2.stream().filter(item -> !list1.contains(item)).collect(Collectors.toList());
+        System.out.println("---得到差集 reduce2 (list2 - list1)---");
+        reduce2.parallelStream().forEach(System.out::println);
+
+        // 并集
+        List<String> listAll = list1.parallelStream().collect(Collectors.toList());
+        List<String> listAll2 = list2.parallelStream().collect(Collectors.toList());
+        listAll.addAll(listAll2);
+        System.out.println("---得到并集 listAll---");
+        listAll.parallelStream().forEach(System.out::println);
+
+        // 去重并集
+        List<String> listAllDistinct = listAll.stream().distinct().collect(Collectors.toList());
+        System.out.println("---得到去重并集 listAllDistinct---");
+        listAllDistinct.parallelStream().forEach(System.out::println);
+
+        System.out.println("---原来的List1---");
+        list1.parallelStream().forEach(System.out::println);
+        System.out.println("---原来的List2---");
+        list2.parallelStream().forEach(System.out::println);
+    }
+
+}
